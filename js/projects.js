fbdb87b20ecda7a1d0d0af2d2d5f8bc04954f14e
Scott Cooper <18429915+PlasmaDiffusion@users.noreply.github.com>
2024-03-15 10:45:26 -0400

    Robots & Things links to video for now

--- js/projects.js
+++ js/projects.js
@@ -170,7 +170,7 @@ export function makeProjects() {
       7,
       "Robots & Things",
       false,
-      "https://robotsandthingsgame.com/",
+      "https://www.youtube.com/watch?v=pucektir9Qo&ab_channel=Robots%26Things",//"https://robotsandthingsgame.com/",
       null,
       "A 3D platformer game that mixes open exploration and linear levels together.",
       "I'm the programmer for this project, using the Unreal engine. I also have done a large chunk of the level design and overall game design.",

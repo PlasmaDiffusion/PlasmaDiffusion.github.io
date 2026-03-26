ee5fc48e3485250d5797d04e41a0beb417fa1959
Scott Cooper <Scott50000@gmail.com>
2026-03-26 16:20:14 -0400

    Slightly shorten physio advice preview description

--- js/projects.js
+++ js/projects.js
@@ -154,7 +154,7 @@ export function getProjectData() {
       false,
       null,
       "https://github.com/PlasmaDiffusion/StretchesV2",
-      "A React Native app that serves as a list and timer for stretch routines, plus physiotherapy advice via OpenAI which uses RAG to fetch info from relevant articles. You can edit this list, change the timer per stretch, and link references to stretches.",
+      "A React Native app that serves as a list and timer for stretch routines, plus physiotherapy advice via OpenAI which uses RAG to fetch from relevant articles. You can edit this list, change the timer per stretch, and link references to stretches.",
       "Additionally there's a Flask backend for handling the advice. It pulls information from PubMed Central.",
 
       null,
